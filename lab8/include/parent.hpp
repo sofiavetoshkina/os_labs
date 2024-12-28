a1ab2c6d994babfe62ff2ba2edb28c1c4da122a0
sofiavetoshkina <sofia@vetoshkin.ru>
2024-12-28 17:12:31 +0300

    add docs

--- lab8/include/parent.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef OS_LABS_PARENT_H
-#define OS_LABS_PARENT_H
-
-#include <iostream>
-#include <string>
-
-void ParentRoutine(const std::string& fileName, std::ostream& output);
-
-#endif //OS_LABS_PARENT_H
